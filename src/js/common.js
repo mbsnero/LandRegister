bf9a071eb626e6f24e5450df0ec104796f01692c
Vlad Bazhanov <bazhanvlad58@gmail.com>
2019-10-24 09:47:41 +0300

    Добавляет селекеторы и чекбоксы на страницу с карточкой, табы

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,7 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
+//= ../../node_modules/jquery-ui-dist/jquery-ui.min.js
 //= select.js
+
+$(document).ready(function() {
+  $( "#tabs" ).tabs(); 
+});
