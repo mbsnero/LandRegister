23cf40c439b549d28a53dcaaf044733a1137360f
mbsnero <mbsnero@gmail.com>
2019-11-02 21:06:33 +0300

    fix struct

--- src/js/common.js
+++ src/js/common.js
@@ -3,26 +3,8 @@
 //= menu.js
 //= select.js
 //= tabs.js
+//= card-acc.js
 
 $(document).ready(function() {
-
-  //accardeon on card
-  
-  var accardeonSrc = [
-    'images/accordion_arr_up_hov.svg',
-    'images/accordion_arr_dw.svg'
-  ]
-  var src;
-
-  $('.title-block__btn, .title-block__text').on('click', function(event) {
-    if ($(event.target).hasClass('title-block__btn')) {
-      $(this).toggleClass('animated');
-    } else if ($(event.target).hasClass('title-block__text')) {
-      $(this).next().toggleClass('animated');
-    }
-
-    $(event.target).parent('.title-block').next().slideToggle();
-  })
-
   
 });
